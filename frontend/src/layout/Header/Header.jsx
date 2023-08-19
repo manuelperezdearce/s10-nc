99c6b9fc8f0b6a6c7b9f14567ea514d1d932c3ed
guille <guillermoneculqueo@gmail.com>
2023-08-18 23:30:58 -0300

    directorios actualizado

--- frontend/src/layout/Header/Header.jsx
+++ frontend/src/layout/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Header = () => {
+const Header = () => { 
   return (
     <div className='w-full h-[80px] bg-[#0a192f] text-white flex justify-center items-center font-bold text-xl uppercase text-center shadow-lg shadow-[#0a192f]'>Header</div>
   )
