625aeb8861b1e3afff7dc0f0c90ee57eedc87e85
guille <guillermoneculqueo@gmail.com>
2023-08-19 00:48:15 -0300

    arreglando tailwind

--- frontend/src/layout/Main/Main.jsx
+++ frontend/src/layout/Main/Main.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello!
+      </h1>
+    </div>
   )
 }
 
