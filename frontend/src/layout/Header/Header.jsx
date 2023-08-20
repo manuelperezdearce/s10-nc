50653ce48d5a5feb895e57b98e8e2a437fd0bc93
guille <guillermoneculqueo@gmail.com>
2023-08-20 05:19:55 -0300

    comienzo nav

--- frontend/src/layout/Header/Header.jsx
+++ frontend/src/layout/Header/Header.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Header = () => {
+  const [isOpem, setIdOpen] = useState(false)
+
+  const handleToggle = () => {
+    setIdOpen(!isOpem)
+  }
+
   return (
-    <div className='w-full h-[80px] bg-lightBrownCustom text-white flex justify-center items-center font-bold text-xl text-[titulo] uppercase text-center shadow-sm '>Header</div>
+    <header className='fixed top-0 -left-[400px] lg:left-0 w-screen h-[80px] bg-lightBrownCustom text-white flex justify-center items-center font-bold text-xl text-[titulo] uppercase text-center shadow-sm '>
+      <div className='w-full h-[80px] bg-slate-400 top-0 left-0'>
+        <nav />
+      </div>
+    </header>
   )
 }
 
--- frontend/src/layout/Main/Main.jsx
+++ frontend/src/layout/Main/Main.jsx
@@ -3,7 +3,7 @@ import Counter from '../../components/counter/Counter'
 
 const Main = () => {
   return (
-    <div className='w-full h-[500px] bg-greenCustom overflow-hidden p-[1rem] box-border flex flex-col justify-center items-center content-center gap-[2rem]'>
+    <div className='w-full h-screen bg-greenCustom overflow-hidden p-[1rem] box-border flex flex-col justify-center items-center content-center gap-[2rem]'>
       <h1 className='w-full p-[1rem] text-3xl font-bold underline text-center'>
         Hola!
       </h1>
