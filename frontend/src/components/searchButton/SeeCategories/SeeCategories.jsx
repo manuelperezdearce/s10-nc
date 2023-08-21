fb66154ad06a736c3f47e78302165677d04f9641
manuelperezdearce <manuel.perezdearce@gmail.com>
2023-08-21 11:57:19 -0400

    F/manuel/chore: routing AppRoutes on main, then rendering main on App, 1 home

---
+++ frontend/src/components/searchButton/SeeCategories/SeeCategories.jsx
@@ -0,0 +1,8 @@
+
+export default function SeeCategories() {
+    return (
+        <section>
+            <h1>Sort by categories</h1>
+        </section>
+    )
+}
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import SeeCategories from '../../components/searchButton/SeeCategories/SeeCategories'
+
 const Home = () => {
   return (
     <div className='mt-[100px] w-full h-[400px] bg-lightBrownCustom grid place-content-center text-blackCustom font-parrafo text-[2rem]'>
       Home
+      <SeeCategories />
     </div>
   )
 }
