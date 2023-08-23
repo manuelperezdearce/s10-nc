61083b2c4094b5c34fc49e071a4dcfef9b925b53
guille <guillermoneculqueo@gmail.com>
2023-08-22 21:09:54 -0300

    diario commit guille

--- frontend/src/layout/Main/Main.jsx
+++ frontend/src/layout/Main/Main.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <main className='main w-full h-screen  bg-greenCustom overflow-hidden p-[1rem] box-border flex flex-col justify-center items-center content-center gap-[2rem]'>
-      <h1 className='w-full p-[1rem] text-3xl font-bold underline text-center'>
-        Hola!
-      </h1>
+    <main className='main w-full h-screen  bg-greenCustom overflow-hidden box-border flex flex-col justify-start items-center content-center gap-[2rem]'>
+      <section className='banner w-screen min-h-[calc(100vh)] bg-orange-500 flex flex-col justify-start items-center content-center'>
+        <div className='bannerContainer w-screen h-[calc(100%-110px)]'>
+          <section className='w-screen h-1/2 bg-purple-400'>
+            <h1>¡Bienvenido a <span>GreenEats Delivery</span></h1>
+            <h2> Tu conexión con la comida saludable y sostenible directamente en tu hogar!</h2>
+          </section>
+          <section className='w-screen h-1/2 bg-slate-400' />
+        </div>
+      </section>
       <button className='botonPruebas'>Boton de pruebas</button>
     </main>
   )
