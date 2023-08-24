ea4346ebf59f8b0adc49442788c8ccfba6781a04
guille <guillermoneculqueo@gmail.com>
2023-08-24 20:09:38 -0300

    banner responsive pruebas

--- frontend/src/components/banner/Banner.jsx
+++ frontend/src/components/banner/Banner.jsx
@@ -12,7 +12,7 @@ import './swipperPersonalizado.css'
 
 const Banner = () => {
   return (
-    <section className='banner w-screen top-0 h-[calc(100vh-110px)] flex justify-center items-center content-center'>
+    <section className='banner w-screen top-0 h-[100vh] flex justify-center items-center content-center'>
       <Swiper
       // install Swiper modules
         modules={[Navigation, Pagination, A11y, EffectFade, Autoplay]}
@@ -26,7 +26,7 @@ const Banner = () => {
         // navigation
         pagination={{ clickable: true }}
         autoplay={{
-          delay: 9000,
+          delay: 90000,
           disableOnInteraction: false
         }}
         onSwiper={(swiper) => console.log(swiper)}
@@ -34,12 +34,12 @@ const Banner = () => {
         className='mySwiper w-screen h-[100%]'
       >
         <SwiperSlide className=' bg-greenCustom'>
-          <div className='absolute top-0 left-0 w-[100%] h-[100%] flex flex-col lg:flex-row lg:items-center text-whiteCustom'>
-            <section className='w-full h-1/2 lg:w-1/2 lg:h-[100%] flex flex-col justify-center items-start p-[1.5rem] gap-3 lg:gap-6'>
+          <div className='absolute top-0 left-0 w-[100%] h-[100%] flex flex-col-reverse lg:flex-row lg:items-center text-whiteCustom'>
+            <section className='w-full h-1/2 lg:w-1/2 lg:h-[100%] flex flex-col justify-center items-start p-[1.5rem] lg:p-[3rem] gap-3 sm:gap-5 lg:gap-6'>
               <h1 className='font-titulo font-bold text-3xl lg:text-5xl'>Bienvenido a <span className='text-oranjeCustom'>GreenEats Delivery</span>!!!</h1>
               <h3 className='font-parrafo font-bold text-base lg:text-xl'>Tu conexión con la comida saludable y sostenible directamente en tu hogar!</h3>
               <p className='font-parrafo font-normal text-sm lg:text-lg'>Diseñada para satisfacer tus deseos de una alimentación consciente sin comprometer el sabor ni la comodidad. Déjanos guiarte a través de un mundo de opciones frescas y nutritivas.</p>
-              <button className='botonBase'>ver seccion</button>
+              <button className='botonBase ' style={{ marginTop: '.5rem' }}>ver seccion</button>
             </section>
             <section className='bgImage w-[100%] h-1/2 lg:w-1/2 lg:h-[100%]'>
               <img className='w-full h-full object-cover' src='https://res.cloudinary.com/dpiwmbsog/image/upload/v1692752160/fitnessMatket/fut_banner35_lvboks.jpg' alt='fondo verduras saludables' />
@@ -47,8 +47,8 @@ const Banner = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide className='bg-[#203a0b] relative'>
-          <div className='absolute top-0 left-0 w-[100%] h-[100%] flex flex-col lg:flex-row lg:items-center text-whiteCustom'>
-            <section className='w-full h-1/2 lg:w-1/2 lg:h-[100%] flex flex-col justify-center items-start p-[1.5rem] gap-3 lg:gap-6'>
+          <div className='absolute top-0 left-0 w-[100%] h-[100%] flex flex-col-reverse lg:flex-row lg:items-center text-whiteCustom'>
+            <section className='w-full h-1/2 lg:w-1/2 lg:h-[100%] flex flex-col justify-center items-start p-[1.5rem] lg:p-[3rem] gap-3 sm:gap-5 lg:gap-6'>
               <h2 className='font-titulo font-bold text-3xl lg:text-5xl'>Explora Nuestro menú saludble!!!</h2>
               <h3 className='font-parrafo font-bold text-base lg:text-xl'>Sumérgete en un paraíso de opciones que te nutrirán y energizarán.</h3>
               <p className='font-parrafo font-normal text-sm lg:text-lg'>Desde platos vegetarianos exquisitos hasta creaciones veganas llenas de sabor, nuestra variedad es un reflejo de la naturaleza misma.</p>
@@ -60,8 +60,8 @@ const Banner = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide className='bg-[#c23e1a] relative'>
-          <div className='absolute top-0 left-0 w-[100%] h-[100%] flex flex-col lg:flex-row lg:items-center text-whiteCustom'>
-            <section className='w-full h-1/2 lg:w-1/2 lg:h-[100%] flex flex-col justify-center items-start p-[1.5rem] gap-3 lg:gap-6'>
+          <div className='absolute top-0 left-0 w-[100%] h-[100%] flex flex-col-reverse lg:flex-row lg:items-center text-whiteCustom'>
+            <section className='w-full h-1/2 lg:w-1/2 lg:h-[100%] flex flex-col justify-center items-start p-[1.5rem] lg:p-[3rem] gap-3 sm:gap-5 lg:gap-6'>
               <h2 className='font-titulo font-bold text-3xl lg:text-5xl'>Tu Comida, Tu Elección!!!</h2>
               <h3 className='font-parrafo font-bold text-base lg:text-xl'>Personaliza tu pedido para que se adapte perfectamente a tus gustos y necesidades.</h3>
               <p className='font-parrafo font-normal text-sm lg:text-lg'>¿Tienes restricciones alimentarias o preferencias específicas? No hay problema. Hacemos que tu comida sea tuya en todos los sentidos.</p>
