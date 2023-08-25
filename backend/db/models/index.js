e5a675e101278a7cb29986dd92f3d4575da35392
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-08-24 23:10:02 -0400

    add: Customer model

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -1,14 +1,29 @@
-
+const { User, UserSchema } = require('./user.model');
+const { Role, RoleSchema } = require('./role.model');
+const { Customer, CustomerSchema } = require('./customer.model');
+const { Restaurant, RestaurantSchema } = require('./restaurant.model');
+const { Meal, MealSchema } = require('./product.model');
 const { Category, CategorySchema } = require('./category.model');
-
-
+const { Order, OrderSchema } = require('./order.model');
+const { OrderItem, OrderItemSchema } = require('./order-item.model');
 
 function setupModels(sequelize) {
-
+  User.init(UserSchema, User.config(sequelize));
+  Role.init(RoleSchema, Role.config(sequelize));
+  Customer.init(CustomerSchema, Customer.config(sequelize));
+  Restaurant.init(RestaurantSchema, Restaurant.config(sequelize));
+  Meal.init(MealSchema, Meal.config(sequelize));
   Category.init(CategorySchema, Category.config(sequelize));
+  Order.init(OrderSchema, Order.config(sequelize));
+  OrderItem.init(OrderItemSchema, OrderItem.config(sequelize));
 
-  // Category.associate(sequelize.models);
-
+  User.associate(sequelize.models);
+  Role.associate(sequelize.models);
+  Customer.associate(sequelize.models);
+  Restaurant.associate(sequelize.models);
+  Category.associate(sequelize.models);
+  Meal.associate(sequelize.models);
+  Order.associate(sequelize.models);
 }
 
 module.exports = setupModels;
