8f8d1464d76a96902f905672ca7bdab64a9ee23e
guille <guillermoneculqueo@gmail.com>
2023-08-25 14:44:57 -0300

    lista de restaurantes, refactorizacion

--- frontend/src/pages/restaurant/Restaurant.jsx
+++ frontend/src/pages/restaurant/Restaurant.jsx
@@ -8,7 +8,9 @@ const ListOfRestaurant = [
   { _id: '1', image_url: 'https://res.cloudinary.com/dpiwmbsog/image/upload/v1692745587/fitnessMatket/rest4_qcdlvq.jpg', name: 'Casa Blanca', description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore esse asperiores quae dicta, deserunt officiis cupiditate debitis tenetur ea et.', ciudad: 'bs As', direccion: 'Av. Rivadavia 1901, C1033 AAJ, Buenos Aires', especiality: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore esse asperiores quae dicta, deserunt officiis cupiditate debitis tenetur ea et.', phone: '2944666666', email: 'mail@gmail.com', city: 'CABA', address: 'Av. S. Martín 3305, C1416CSI CABA' },
   { _id: '2', image_url: 'https://res.cloudinary.com/dpiwmbsog/image/upload/v1692745587/fitnessMatket/rest5_pbyngs.png', name: 'Buenos Aires Restaurant', description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore esse asperiores quae dicta, deserunt officiis cupiditate debitis tenetur ea et.', ciudad: 'Buenos Aires', direccion: 'ABC, Carlos Pellegrini 111, C1009 C1009ABC, Buenos Aires', especiality: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore esse asperiores quae dicta, deserunt officiis cupiditate debitis tenetur ea et.', phone: '2944666666', email: 'mail@gmail.com', city: 'CABA', address: 'Av. S. Martín 3305, C1416CSI CABA' },
   { _id: '3', image_url: 'https://res.cloudinary.com/dpiwmbsog/image/upload/v1692745587/fitnessMatket/rest1_kfyb0q.jpg', name: 'El Mercado', description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore esse asperiores quae dicta, deserunt officiis cupiditate debitis tenetur ea et.', ciudad: 'Buenos Aires', direccion: 'Calle, Martha Salotti 445, Buenos Aires', especiality: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore esse asperiores quae dicta, deserunt officiis cupiditate debitis tenetur ea et.', phone: '2944666666', email: 'mail@gmail.com', city: 'CABA', address: 'Av. S. Martín 3305, C1416CSI CABA' },
-  { _id: '4', image_url: 'https://res.cloudinary.com/dpiwmbsog/image/upload/v1692745587/fitnessMatket/rest2_o2sp8o.jpg', name: 'El Motivo RestoBar', description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore esse asperiores quae dicta, deserunt officiis cupiditate debitis tenetur ea et.', ciudad: 'Buenos Aires', direccion: 'Av. S. Martín 3305, C1416CSI CABA', especiality: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore esse asperiores quae dicta, deserunt officiis cupiditate debitis tenetur ea et.', phone: '2944666666', email: 'mail@gmail.com', city: 'CABA', address: 'Av. S. Martín 3305, C1416CSI CABA' }
+  { _id: '4', image_url: 'https://res.cloudinary.com/dpiwmbsog/image/upload/v1692745587/fitnessMatket/rest2_o2sp8o.jpg', name: 'El Motivo RestoBar', description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore esse asperiores quae dicta, deserunt officiis cupiditate debitis tenetur ea et.', ciudad: 'Buenos Aires', direccion: 'Av. S. Martín 3305, C1416CSI CABA', especiality: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore esse asperiores quae dicta, deserunt officiis cupiditate debitis tenetur ea et.', phone: '2944666666', email: 'mail@gmail.com', city: 'CABA', address: 'Av. S. Martín 3305, C1416CSI CABA' },
+  { _id: '5', image_url: 'https://res.cloudinary.com/dpiwmbsog/image/upload/v1692745587/fitnessMatket/rest1_kfyb0q.jpg', name: 'El Mercado', description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore esse asperiores quae dicta, deserunt officiis cupiditate debitis tenetur ea et.', ciudad: 'Buenos Aires', direccion: 'Calle, Martha Salotti 445, Buenos Aires', especiality: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore esse asperiores quae dicta, deserunt officiis cupiditate debitis tenetur ea et.', phone: '2944666666', email: 'mail@gmail.com', city: 'CABA', address: 'Av. S. Martín 3305, C1416CSI CABA' },
+  { _id: '6', image_url: 'https://res.cloudinary.com/dpiwmbsog/image/upload/v1692745587/fitnessMatket/rest2_o2sp8o.jpg', name: 'El Motivo RestoBar', description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore esse asperiores quae dicta, deserunt officiis cupiditate debitis tenetur ea et.', ciudad: 'Buenos Aires', direccion: 'Av. S. Martín 3305, C1416CSI CABA', especiality: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore esse asperiores quae dicta, deserunt officiis cupiditate debitis tenetur ea et.', phone: '2944666666', email: 'mail@gmail.com', city: 'CABA', address: 'Av. S. Martín 3305, C1416CSI CABA' }
 ]
 
 const Restaurant = () => {
@@ -21,17 +23,23 @@ const Restaurant = () => {
   }, [])
 
   return (
-    <div
-      ref={restaurantRef}
-      className='mt-[100px] w-full min-h-[400px] bg-lightBrownCustom flex flex-wrap justify-center items-center gap-4 text-blackCustom font-parrafo text-[2rem] p-10'
-    >
-      {
+    <section className='containerRestaurants lg:mt-[130px] w-full my-11 min-h-[400px] flex flex-col justify-start items-center content-center bg-whiteCustom gap-6'>
+      <section className='contentText px-[1rem] py-[1rem] flex flex-col gap-2 max-w-[1500px] '>
+        <h2 className='font-titulo text-3xl font-bold text-marronCustom'>Descubre Nuestra Selección de Restaurantes Saludables</h2>
+        <p className='font-parrafo font-normal text-sm lg:text-base text-marronCustom'>Nuestra plataforma te conecta con una selección de <strong>restaurantes</strong> que comparten nuestra pasión por la <strong>comida saludable</strong> y sostenible. Cada restaurante en nuestra lista ha sido cuidadosamente elegido para ofrecerte una <strong>experiencia gastronómica única</strong> que cuida tanto de tu bienestar como del planeta.</p>
+      </section>
+      <section
+        ref={restaurantRef}
+        className='min-h-[500px] w-[100%] flex flex-wrap justify-center items-center gap-4 text-blackCustom font-parrafo text-[2rem] py-[1rem] max-w-[1500px]'
+      >
+        {
         ListOfRestaurant.map((restaurant) => {
           return <RestaurantCard key={restaurant._id} object={restaurant} />
         })
       }
 
-    </div>
+      </section>
+    </section>
   )
 }
 
