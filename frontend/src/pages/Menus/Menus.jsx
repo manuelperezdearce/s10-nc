817a90af58318d9543684f02e7f227ad2c7963b4
guille <guillermoneculqueo@gmail.com>
2023-08-27 20:33:59 -0300

    menus

--- frontend/src/pages/Menus/Menus.jsx
+++ frontend/src/pages/Menus/Menus.jsx
@@ -12,12 +12,18 @@ const Menus = () => {
   }, [])
 
   return (
-    <div
+
+    <section
       ref={menuRef}
-      className='mt-[100px] w-full h-[400px] bg-lightBrownCustom grid place-content-center text-blackCustom font-parrafo text-[2rem]'
+      className='containerRestaurants lg:mt-[130px] w-full my-11 min-h-[400px] flex flex-col justify-start items-center content-center bg-whiteCustom gap-6 border-2 border-r-emerald-200 '
     >
-      Menu
-    </div>
+      <section className='contentText px-[1rem] py-[1rem] flex flex-col gap-2 max-w-[1500px] '>
+        <h2 className='font-titulo text-3xl font-bold text-marronCustom'>Descubre Nuestra Selección de Platos y bebidas.</h2>
+        <p className='font-parrafo font-normal text-sm lg:text-base text-marronCustom'>
+          Nuestra selección de platos te brinda una experiencia culinaria única, con opciones saludables y deliciosas para cada etapa de tu comida. Desde entradas frescas hasta postres indulgentes, estamos comprometidos con tu bienestar y satisfacción gastronómica.
+        </p>
+      </section>
+    </section>
   )
 }
 
