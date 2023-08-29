191befacdc1930587c6df53896006b5a50c54b47
Henry Milac <henrymilacprog@gmail.com>
2023-08-29 09:57:57 -0500

    First Advanced

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -5,6 +5,7 @@ import Home from '../pages/home/Home'
 import Restaurant from '../pages/restaurant/Restaurant'
 import Menus from '../pages/Menus/Menus'
 import Contact from '../pages/Contact/Contact'
+import RestaurantID from '../pages/restaurant/RestaurantID'
 
 const AppRoutes = () => {
   return (
@@ -14,6 +15,7 @@ const AppRoutes = () => {
 
       {/* <Route path='/home' element={<Home />} /> */}
       <Route path='/restaurant' element={<Restaurant />} />
+      <Route path='/restaurant/id' element={<RestaurantID />} />
       <Route path='/menus' element={<Menus />} />
       <Route path='/contact' element={<Contact />} />
       <Route path='*' element={<NotFound />} />
