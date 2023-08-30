53b00f93e497086f089aa572c547085932f7ac13
guille <guillermoneculqueo@gmail.com>
2023-08-29 23:09:16 -0300

    logica nav search prueba

--- frontend/src/components/searchButton/SearchButton.jsx
+++ frontend/src/components/searchButton/SearchButton.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom'
 import React, { useEffect, useState } from 'react'
 import { IoSearch } from 'react-icons/io5'
 import './searchButton.css'
@@ -7,34 +8,54 @@ import filtersProductsByKeyword from '../../hooks/filtersProductsByKeyword'
 
 const SearchButton = () => {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   const foods = useSelector((state) => state.foods.data)
-
   const [searchTerm, setSearchTerm] = useState('')
+  const [redirect, setRedirect] = useState(false)
 
   useEffect(() => {
     dispatch(getFoods())
   }, [dispatch])
 
+  useEffect(() => {
+    if (redirect === true) {
+      setTimeout(() => {
+        navigate('/search')
+        setRedirect(false)
+      }, 2000)
+    }
+  }, [redirect])
+
   const handleSearch = (e) => {
     const filteredData = filtersProductsByKeyword(foods, searchTerm)
     dispatch(addToproductsByKeyword(filteredData))
     console.log('Foods Filter Keyword ->', filteredData)
+    setRedirect(true)
   }
 
   return (
     <div className='containerSear'>
-      <input
-        type='text'
-        name='text'
-        className='input'
-        inputProps={{ 'aria-label': 'Buscar Por Palabra' }}
-        value={searchTerm}
-        onChange={(event) => setSearchTerm(event.target.value)}
-        placeholder='Buscador'
-      />
-      <button className='icon' onClick={handleSearch}>
-        <IoSearch />
-      </button>
+      {
+        redirect
+          ? (
+            <p className='redirect '>redirigiendo...</p>
+            )
+          : (
+            <>
+              <input
+                type='text'
+                name='text'
+                className='input'
+                value={searchTerm}
+                onChange={(event) => setSearchTerm(event.target.value)}
+                placeholder='Buscador'
+              />
+              <button className='icon' onClick={handleSearch}>
+                <IoSearch />
+              </button>
+            </>
+            )
+      }
     </div>
   )
 }
