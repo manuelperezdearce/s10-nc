d0143498219f561725d935cf81da27ce6a482a98
guille <guillermoneculqueo@gmail.com>
2023-08-30 13:39:19 -0300

    search nav menu + lista de productos

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -5,6 +5,7 @@ import Home from '../pages/home/Home'
 import Restaurant from '../pages/restaurant/Restaurant'
 import Menus from '../pages/Menus/Menus'
 import Contact from '../pages/Contact/Contact'
+import ListOfProducts from '../pages/ListOfProducts/ListOfProducts'
 
 const AppRoutes = () => {
   return (
@@ -16,6 +17,7 @@ const AppRoutes = () => {
       <Route path='/restaurant' element={<Restaurant />} />
       <Route path='/menus' element={<Menus />} />
       <Route path='/contact' element={<Contact />} />
+      <Route path='/search' element={<ListOfProducts />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
   )
