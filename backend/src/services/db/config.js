9676eac08a79950bef6ae29d339216ab0b9a174b
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-09-05 19:56:04 -0300

    test: deploy on render

--- backend/src/services/db/config.js
+++ backend/src/services/db/config.js
@@ -1,14 +1,17 @@
 const { Sequelize } = require('sequelize');
+const { config } = require('../../config/env');
 
-const sequelize = new Sequelize(
-  "test_sequelize",
-  "postgres",
-  "Guille04",
-  {
-    host: "localhost",
-    dialect: 'postgres',
-    logging: false,
-  },
-);
+// const sequelize = new Sequelize(
+//   "test_sequelize",
+//   "postgres",
+//   "Guille04",
+//   {
+//     host: "localhost",
+//     dialect: 'postgres',
+//     logging: false,
+//   },
+// );
+
+const sequelize = new Sequelize(config.dbUrl)
 
 module.exports = sequelize;
