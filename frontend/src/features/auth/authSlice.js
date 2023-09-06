8de3faa81159eb61c8a04595128a78dd366b7e25
guille <guillermoneculqueo@gmail.com>
2023-09-05 22:45:52 -0300

    algunos cambios de prueba

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,8 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const initialState = {
+  user: null,
+  isLoggedIn: false,
+  isLoading: false,
+  error: null,
+  token: null, // si hay token user login
+  isAdmin: false,
+  isRestaurant: false,
+  isNormy: false
+}
+
 export const authSlice = createSlice({
   name: 'Auth',
-  initialState: {},
+  initialState,
   reducers: {}
 })
 
