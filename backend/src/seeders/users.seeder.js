021a16a8672ff757541ed7891730cf03ef8849de
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-09-06 23:47:35 -0300

    fix: user seeder

--- backend/src/seeders/users.seeder.js
+++ backend/src/seeders/users.seeder.js
@@ -2,18 +2,17 @@ const { User } = require('../services/db/models/user.model');
 const bcrypt = require('bcrypt');
 
 const createUsers = async () => {
-  const createPassword = (pass) => bcrypt.hash(pass, 10);
+  const pass = bcrypt.hash('ensaladas123', 10);
   try {
-    // await User.sync({ force: true });
 
     await User.create({
       email: 'vaquitas@mail.com',
-      password: createPassword('ensaladas123'),
+      password: pass,
       role_id: 2,
     });
     await User.create({
       email: 'customer@juan.com',
-      password: createPassword('ensaladas123'),
+      password: pass,
       role_id: 1,
     });
 
