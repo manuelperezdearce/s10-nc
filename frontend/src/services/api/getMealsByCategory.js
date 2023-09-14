69ef79db20f805585b1cb6809bab3f3524d1bce5
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-09-14 17:47:54 -0300

    Meals by category page

--- frontend/src/services/api/getMealsByCategory.js
+++ frontend/src/services/api/getMealsByCategory.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import { endpoints } from '../../constants/api'
 
-export const getMeals = async (id) => {
-  const meals = await axios.get(`${endpoints.meals}/${id}`)
+export const getMealsByCategory = async (id) => {
+  const meals = await axios.get(`${endpoints.meals}/search?category_id=${id}`)
+  console.log(meals.data)
   return meals.data
 }
-
